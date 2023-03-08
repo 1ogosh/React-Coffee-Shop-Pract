b0400882697b56a3676dad85c271ee1dcd499657
Andrew Salmanov <ads739912@gmail.com>
2023-03-08 15:39:05 +0200

    publicate

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+import Header from '../main-header/main-header';
+
+import './app.scss';
+
+
+export default App;
+
+class App extends Component {
+
+}
+
---
+++ src/components/main-header/main-header.js
@@ -0,0 +1,22 @@
+
+import './main-header.scss';
+
+const Header = () => {
+        return (
+                <div className="header">
+                        <div className="nav">
+                                <ul>
+                                        <li>Coffee house</li>
+                                        <li>Our coffee</li>
+                                        <li>For your pleasure</li>
+                                </ul>
+                        </div>
+                        <div className="main">
+                                <h1>Everything You Love About Coffee</h1>
+                                <img src="" alt="" />
+                                <h2>We makes every day full of energy and taste <br /> Want to try our beans?</h2>
+                                <button type="">More</button>
+                        </div>
+                </div>
+        )
+}
